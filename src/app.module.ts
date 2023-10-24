b85e4e27d895d6a881f3b2b3cfe3087f65b22061
aayush-dhakal <aayushdhakal77@gmail.com>
2023-10-24 23:44:57 +0545

    added config module for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [QuizModule, TypeOrmModule.forRoot(typeOrmConfig)], // our main app has to know about all other modules
+  // imports: [QuizModule, TypeOrmModule.forRoot(typeOrmConfig)], // our main app has to know about all other modules
+  imports: [
+    QuizModule,
+    ConfigModule.forRoot({ isGlobal: true }), // we need this to read environment variables in typeorm config
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+  ], // our main app has to know about all other modules
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,35 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'SuperSecret!23',
-  database: 'quiz',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
-  logging: true, // this will log all the raw sql queries
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    console.log("configService.get('DB_HOST')", configService.get('DB_HOST'));
+    return {
+      type: 'postgres',
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'postgres',
+      // password: 'SuperSecret!23',
+      // database: 'quiz',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+      logging: true, // this will log all the raw sql queries
+    };
+  }
+}
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
 };
